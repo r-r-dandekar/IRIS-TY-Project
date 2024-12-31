db46bf765f8cf7a0f0bdec3e2208c2b4e4eb351d
r-r-dandekar <radhan.r.dandekar@gmail.com>
2024-12-31 17:15:09 +0530

    stuffff including facerecognition but not tested

--- python/image_captioning.py
+++ python/image_captioning.py
@@ -5,6 +5,7 @@ import torch
 from PIL import Image
 
 model = VisionEncoderDecoderModel.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
+print("HII")
 feature_extractor = ViTImageProcessor.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
 tokenizer = AutoTokenizer.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
 
